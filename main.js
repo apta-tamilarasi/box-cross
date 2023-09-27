5e2f08ad1532f82d94fb036d9dd681fa82efef6a
apta-tamilarasi <athinarayanantamilarasi@gamil.com>
2023-09-27 07:55:59 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,29 @@
+let a=parseInt(prompt("enter the start value"))
+
+
+for(i=1;i<=a;i++){
+	document.write("*&nbsp")
+}
+document.write("<br>")
+
+for(i=1;i<a-1;i++){
+for(j=1;j<=a;j++){
+	if( j==1+i|| j==a-i)
+		document.write("*&nbsp")
+	
+
+	else
+		document.write("&nbsp&nbsp&nbsp")
+	
+	
+}
+document.write("<br>")
+
+
+	
+}
+
+
+for(i=1;i<=a;i++){
+	document.write("*&nbsp")
+}
